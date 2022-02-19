eca93706db8aa6ef0286341cc73cb49531c42c4a
Moie <silivanov007@mail.ru>
2022-02-19 13:52:47 +0300

    Создание полей ввода ифнормации

--- main.py
+++ main.py
@@ -2,6 +2,19 @@ from tkinter import *
 
 root = Tk()
 root.title("Показания электросчетчика")
-root.geometry("400x300")
+root.geometry("700x350")
+
+label1 = Label(root, text = "Введите дату снятия показаний в формате: 01.01.2000")
+label1.place(relx=0.09, rely=0.08)
+
+label1 = Label(root, text = "Введите показания счетчика в формате: 123.7")
+label1.place(relx=0.6, rely=0.08)
+
+entry1 = Entry(root)
+entry1.place(relheight=0.07, relwidth=0.2, relx=0.2, rely=0.2)
+entry1.focus()
+
+entry2 = Entry(root)
+entry2.place(relheight=0.07, relwidth=0.2, relx=0.7, rely=0.2)
 
 root.mainloop()
